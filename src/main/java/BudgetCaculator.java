a9bb51840fcba6e25b245c8d3484518814e7dfa7
Evan <pifan76@gmail.com>
2018-08-22 21:24:50 +0800

    use sum instead of foreach

--- src/main/java/BudgetCaculator.java
+++ src/main/java/BudgetCaculator.java
@@ -12,19 +12,9 @@ class BudgetCaculator {
 
     double totalAmount(LocalDate start, LocalDate end) {
 
-        List<Budget> budgetList = budgetRepo.getAll();
-
         Period period = new Period(start, end);
-        period.start = start;
-        period.end = end;
 
-        double totalAmount = 0.0;
-
-        for (Budget budget : budgetList) {
-            totalAmount += budget.effectiveTotalAmount(period);
-        }
-
-        return totalAmount;
+        return budgetRepo.getAll().stream().mapToDouble(b -> b.effectiveTotalAmount(period)).sum();
 
     }
 }
